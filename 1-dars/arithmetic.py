c702ca160e41d21d6b6cbcebf76461eecd29da0c
AsadulloAkramov <asadullo.2807@gmail.com>
2022-04-16 09:43:33 +0500

    some arithmetric operations added

---
+++ 1-dars/arithmetic.py
@@ -0,0 +1,18 @@
+# Pythondagi asosiy arifmetik amallar
+#Qo'shish
+print(5+2)
+
+#Ayirish
+print(5-2)
+
+#Ko'paytirish
+print(5*2)
+
+#Bo'lish 
+print(5/2)
+
+#Kvadrat darajaga oshirish
+print(5**2)
+
+#Qoldiqli bo'lish
+print(5%2)
