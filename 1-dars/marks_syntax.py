2832e130242be5b3039a8d863f2cfd71d8f02a8f
AsadulloAkramov <asadullo.2807@gmail.com>
2022-04-16 09:39:27 +0500

    double and single quote mark syntax added

---
+++ 1-dars/marks_syntax.py
@@ -0,0 +1,12 @@
+#Birtirnoqdan foydalanish
+print('Hayrli tong!')
+#Men "Dell" markasidagi noutbuk sotib oldim ni consolega chiqarib ko'ramiz
+print('Men "Dell" markasidagi noutbuk sotib oldim')
+
+# Agar matnni bir necha qatorga bo'lib yozish talab etilsa unda 
+# uni """  yoki ''' uchtalik qo'shtirnoq yoki birtirnoq dan foydalanamiz
+print("""Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami """)
+
+# \n syntax yordamida ham qatorga bo'lish mumkin
+print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
